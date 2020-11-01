c9fcb3f1beb9534978e9800ad1721b7fa613d912
William Hall <willhall123@icloud.com>
2020-11-01 05:33:44 -0600

    started debugging

--- app.js
+++ app.js
@@ -10,30 +10,6 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
-
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
-
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
-
 const userInput = [
     {
         type: "list",
@@ -72,19 +48,19 @@ const userInput = [
         type: "input",
         name: "github",
         message: "Github username of new employee?",
-        when: (input) => input.title === "Engineer"
+        when: (input) => input.role === "Engineer"
     },
     {
         type: "input",
         name: "name",
         message: "Office number of new employee?",
-        when: (input) => input.title === "Manager"
+        when: (input) => input.role === "Manager"
     },
     {
         type: "input",
         name: "name",
         message: "New employee school name?",
-        when: (input) => input.title === "Intern"
+        when: (input) => input.role === "Intern"
     }
 
 ];
@@ -95,15 +71,15 @@ function init() {
     prompt(userInput).then(input => {
         if(input.new === "no") {
             return;
-        } else if (input.title === "Engineer") {
+        } else if (input.role === "Engineer") {
             let { id, name, email, github } = input;
             const employee = new Engineer(id, name, email, github);
             team.push(employee);
-        } else if (input.title === "Manager") {
+        } else if (input.role === "Manager") {
             let { id, name, email, officeNumber } = input;
             const employee = new Manager(id, name, email, officeNumber);
             team.push(employee);
-        } else if (input.title === "Intern") {
+        } else if (input.role === "Intern") {
             let { id, name, email, school } = input;
             const employee = new Intern(id, name, email, school);
             team.push(employee);
