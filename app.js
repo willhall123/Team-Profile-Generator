2f68b92a9e54d29c1a781e46fbb63a1c2e4c3f92
William Hall <willhall123@icloud.com>
2020-11-01 04:53:02 -0600

    updated app.js with questions

--- app.js
+++ app.js
@@ -33,3 +33,79 @@ const render = require("./lib/htmlRenderer");
 // for further information. Be sure to test out each class and verify it generates an
 // object with the correct structure and methods. This structure will be crucial in order
 // for the provided `render` function to work! ```
+
+const userInput = [
+    {
+        type: "list",
+        name: "new",
+        message: "Add new employee?",
+        choices: ["yes", "no"],
+        default: "no"
+    },
+    {
+        type: "list",
+        name: "title",
+        message: "Title of new employee?",
+        choices: ["Engineer", "Manager", "Intern"],
+        default: "Intern",
+        when: (input) => input.new === "yes"
+    },
+    {
+        type: "input",
+        name: "id",
+        message: "ID for new employee?",
+        when: (input) => input.new === "yes"
+    },
+    {
+        type: "input",
+        name: "name",
+        message: "Name of new employee?",
+        when: (input) => input.new === "yes"
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "Email address for new employee?",
+        when: (input) => input.new === "yes"
+    },
+    {
+        type: "input",
+        name: "github",
+        message: "Github username of new employee?",
+        when: (input) => input.title === "Engineer"
+    },
+    {
+        type: "input",
+        name: "name",
+        message: "Office number of new employee?",
+        when: (input) => input.title === "Manager"
+    },
+    {
+        type: "input",
+        name: "name",
+        message: "New employee school name?",
+        when: (input) => input.title === "Intern"
+    }
+
+];
+
+init();
+function init() {
+    prompt(userInput).then(input => {
+        if(input.new === "no") {
+            return;
+        } else if (input.title === "Engineer") {
+            let { id, name, email, github } = input;
+            const employee = new Engineer(id, name, email, github);
+            team.push(employee);
+        } else if (input.title === "Manager") {
+            let { id, name, email, officeNumber } = input;
+            const employee = new Manager(id, name, email, officeNumber);
+            team.push(employee);
+        } else if (input.title === "Intern") {
+            let { id, name, email, school } = input;
+            const employee = new Intern(id, name, email, school);
+            team.push(employee);
+        }
+    });
+}
