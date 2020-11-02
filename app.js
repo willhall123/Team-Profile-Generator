fcf021be3c0641b570c9c94b583275ea8dde0b20
William Hall <willhall123@icloud.com>
2020-11-02 05:57:03 -0600

    debugging

--- app.js
+++ app.js
@@ -4,12 +4,9 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
-
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
-
 const render = require("./lib/htmlRenderer");
-
 const userInput = [
     {
         type: "list",
@@ -20,22 +17,21 @@ const userInput = [
     },
     {
         type: "list",
-        name: "title",
-        message: "Title of new employee?",
+        name: "role",
+        message: "Role of new employee?",
         choices: ["Engineer", "Manager", "Intern"],
-        default: "Intern",
-        when: (input) => input.new === "yes"
-    },
-    {
-        type: "input",
-        name: "id",
-        message: "ID for new employee?",
         when: (input) => input.new === "yes"
     },
     {
         type: "input",
         name: "name",
         message: "Name of new employee?",
+        when: (input) => input.new === "yes"
+    },
+    {
+        type: "input",
+        name: "id",
+        message: "ID for new employee?",
         when: (input) => input.new === "yes"
     },
     {
@@ -52,13 +48,13 @@ const userInput = [
     },
     {
         type: "input",
-        name: "name",
+        name: "officeNumber",
         message: "Office number of new employee?",
         when: (input) => input.role === "Manager"
     },
     {
         type: "input",
-        name: "name",
+        name: "school",
         message: "New employee school name?",
         when: (input) => input.role === "Intern"
     }
@@ -68,26 +64,26 @@ const team =[];
 
 init();
 function init() {
-    prompt(userInput).then(input => {
+    inquirer.prompt(userInput).then(input => {
         if(input.new === "no") {
+            const html = render(team);
+            fs.writeFile(outputPath, html, function(err) {
+                if(err) throw err;
+            });
             return;
         } else if (input.role === "Engineer") {
-            let { id, name, email, github } = input;
-            const employee = new Engineer(id, name, email, github);
-            team.push(employee);
+            let { name, id, email, github } = input;
+            const engineer = new Engineer(name, id, email, github);
+            team.push(engineer);
         } else if (input.role === "Manager") {
-            let { id, name, email, officeNumber } = input;
-            const employee = new Manager(id, name, email, officeNumber);
-            team.push(employee);
+            let { name, id, email, officeNumber } = input;
+            const manager = new Manager(name, id, email, officeNumber);
+            team.push(manager);
         } else if (input.role === "Intern") {
-            let { id, name, email, school } = input;
-            const employee = new Intern(id, name, email, school);
-            team.push(employee);
+            let { name, id, email, school } = input;
+            const intern = new Intern(name, id, email, school);
+            team.push(intern);
         }
         init();
-    }).then(() => {
-        fs.writeFile("index.html", render(team), function(err) {
-            if(err) throw err;
-        });
     });
-}
+};
